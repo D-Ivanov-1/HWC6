3fcf236f8472702e597cf5ca35104677a3fd3a4d
Dima Ivanov <ivanovdimat5@gmail.com>
2023-01-17 18:07:29 +0300

    Решение первой задачи

---
+++ Task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿//Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.Clear();
+System.Console.Write("Введите количество планируемых вводимых чисел: \t");
+
+int elementsCount = int.Parse(Console.ReadLine());
+int [] array = new int[elementsCount];
+for (int i = 0; i < array.Length; i++)
+{
+
+    System.Console.Write("Введите число: ");
+    array[i] = int.Parse(Console.ReadLine());   
+    Console.Clear();
+
+}
+
+System.Console.Write($"Числа: ");
+
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write($" {array[i]} ");
+}
+
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
+
+System.Console.WriteLine(sum);
